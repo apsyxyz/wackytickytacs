e640db8ccc3539e6daffc38279fc4ea036207cbb
apsyxyz <whydoesthissuck@cock.lu>
2016-03-17 09:08:54 +0000

    improved support for dynamic pages. bit slow, but can be improved.

--- src/content.js
+++ src/content.js
@@ -1,11 +1,38 @@
-chrome.storage.local.get("wackytictacs",function(items) {
-    var imgs = items["wackytictacs"];
-    var regex = RegExp("\(minion\)|\(Despicable.Me\)","i");
-    var results = regex.exec(document.body.innerHTML);
-    if(results != null){
-        var fun = document.getElementsByTagName("img");
-        for(i = 0;i < fun.length;i++){
-            fun[i].src = imgs[Math.floor(Math.random()*imgs.length)];
+
+/* update the changed elements */
+function update(updated){
+    chrome.storage.local.get("wackytictacs",function(items) {
+        var imgs = items["wackytictacs"];
+        var regex = RegExp("\(minion\)|\(Despicable.Me\)","i");
+        var results = regex.exec(updated.innerHTML);
+        var width = 0;
+        var height = 0;
+        if(results != null){
+            var fun = updated.getElementsByTagName("img");
+            for(i = 0;i < fun.length;i++){
+                /* Save the height/width to avoid messing up the pages */
+                width = fun[i].clientWidth;
+                height = fun[i].clientHeight;
+                /* assign new image */
+                fun[i].src = imgs[Math.floor(Math.random()*imgs.length)];
+                fun[i].width = width;
+                fun[i].height = height;
+            }
         }
-    }
+    });
+};
+
+/* handle page changes */
+var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+              update(mutation.target);
+    });    
 });
+var config = {
+                attributes: true, 
+                subtree: true
+            };
+observer.observe(document.body, config);
+
+/* set up */
+update(document.body);
